ed8629a8d18a29ba31c4f3a242e5735bb6588224
Tanya Pustovetova <plazaleto@yandex.ru>
2022-07-14 21:53:20 +0700

    init commit

---
+++ src/test/java/Tanya/MonthSales/services/StatServiceTest.java
@@ -0,0 +1,39 @@
+package Tanya.MonthSales.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import Tanya.MonthSales.services.StatService;
+import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+
+public class StatServiceTest {
+
+    @Test
+    public void test() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        assertEquals(expected, actual);
+    }
+
+}
+
+
+
+
+
+
+
