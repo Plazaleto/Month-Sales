961c12f1216f9f7e02f3d8f2c78061e167da77f4
Tanya Pustovetova <plazaleto@yandex.ru>
2022-07-17 15:56:41 +0700

    3 commit

--- src/main/java/Tanya/MonthSales/services/StatService.java
+++ src/main/java/Tanya/MonthSales/services/StatService.java
@@ -2,7 +2,7 @@ package Tanya.MonthSales.services;
 
 public class StatService {
 
-    public long sum(long[] sales) {
+    public long sum(long[] sales) {     //Сумма всех продаж
         long sum = 0;
         for (long sale : sales) {
             sum += sale;
@@ -10,11 +10,11 @@ public class StatService {
         return sum;
     }
 
-    public long average(long[] sales) {
+    public long average(long[] sales) {    // Средняя сумма продаж в месяц
         return sum(sales) / 12;
     }
 
-    public int avgLess(long[] sales) {
+    public int avgLess(long[] sales) {    // Кол-во месяцев, в которых продажи были ниже среднего
         long avg = average(sales);
         int count = 0;
         for (long sale : sales) {
@@ -23,6 +23,47 @@ public class StatService {
             }
         }
         return count;
+    }
+
+    public int minSales(long[] sales) {      // Номер месяца, в котором был минимум продаж
+
+        int minMonth = 0;
+        int month = 0;
+
+        for (long sale : sales) {
+            if (sale < sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+
+        return minMonth + 1;
+
+    }
+
+    public int maxSales(long[] sales) {     // Номер месяца, в котором был пик продаж
+
+        int maxMouth = 0;
+        int mouth = 0;
+
+        for (long sale : sales) {
+            if (sale >= sales[maxMouth]) {
+                maxMouth = mouth;
+            }
+            mouth = mouth + 1;
+        }
+        return maxMouth + 1;
+    }
+
+    public int aboveAverage(long[] sales) {   // Кол-во месяцев, в которых продажи были выше среднего
+        int aboveAverageSalesMonth = 0;
+
+        for (long sale : sales) {
+            if (sale > average(sales)) {
+                aboveAverageSalesMonth += 1;
+            }
+        }
+        return aboveAverageSalesMonth;
     }
 
 }
--- src/test/java/Tanya/MonthSales/services/StatServiceTest.java
+++ src/test/java/Tanya/MonthSales/services/StatServiceTest.java
@@ -1,14 +1,14 @@
 package Tanya.MonthSales.services;
 
 import static org.junit.jupiter.api.Assertions.*;
-import Tanya.MonthSales.services.StatService;
+
 import org.junit.jupiter.api.Test;
 //import org.junit.jupiter.api.Test;
 
 public class StatServiceTest {
 
     @Test
-    public void test() {
+    public void testSalesSum() {
         StatService service = new StatService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
@@ -19,7 +19,7 @@ public class StatServiceTest {
     }
 
     @Test
-    public void test2() {
+    public void testAverageSales() {
         StatService service = new StatService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
@@ -28,6 +28,63 @@ public class StatServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+
+    public void testMaxSales() {
+        StatService manager = new StatService();
+
+        long[] sales = {12, 3, 22, 37, 8, 10, 13, 11, 21, 30, 14, 15};
+
+        long actual = manager.maxSales(sales);
+        long expected = 4;
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
+    @Test
+
+    public void testMinSales() {
+        StatService manager = new StatService();
+
+        long[] sales = {12, 3, 22, 37, 8, 10, 13, 11, 21, 30, 14, 15};
+
+        long actual = manager.minSales(sales);
+        long expected = 2;
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
+    @Test
+
+    public void testAboveAverageSales() {
+        StatService manager = new StatService();
+
+        long[] sales = {12, 3, 22, 37, 8, 10, 13, 11, 21, 30, 14, 15};
+
+        long actual = manager.aboveAverage(sales);
+        long expected = 4;
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
+    @Test
+
+    public void testUnderAverageSales() {
+        StatService manager = new StatService();
+
+        long[] sales = {12, 3, 22, 37, 8, 10, 13, 11, 21, 30, 14, 15};
+
+        long actual = manager.avgLess(sales);
+        long expected = 8;
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
 
 }
 
